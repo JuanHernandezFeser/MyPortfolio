31c5c460994ab3a6adcac561f2b3b8ade056107e
Juan Ignacio Hernández Feser <juani.hernandez.feser.96@gmail.com>
2024-04-15 17:23:12 -0300

    cambios en index

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Función para desplazarse suavemente al principio de la página
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+// Mostrar u ocultar el botón al desplazarse
+window.addEventListener('scroll', function() {
+  var scrollTopBtn = document.getElementById('scrollTopBtn');
+  if (window.scrollY > 300) { // Muestra el botón cuando el usuario ha desplazado 300px hacia abajo
+    scrollTopBtn.style.display = 'block';
+  } else {
+    scrollTopBtn.style.display = 'none';
+  }
+});
